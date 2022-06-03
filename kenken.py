98d6bcbf4264216bab90a99da208ddd2482a7cbe
noha-223 <73791682+noha-223@users.noreply.github.com>
2022-06-03 03:40:12 +0200

    Update kenken.py

--- kenken.py
+++ kenken.py
@@ -183,3 +183,189 @@ class KenKen():
 
             return constraintA and constraintB
         		
+  def constraint_op(self, variable, value, block_Num):
+        if self.block_Op[block_Num] == "''":
+            return value == self.block_Val[block_Num]
+    
+        elif self.block_Op[block_Num] == 'add':
+            sum = 0
+            num_Assignment2 = 0
+            # we go through to each var and in the block 
+            # and check if it has on value in his domain 
+            # if it's we just sum it, if not we don't return value of it    
+            for V in self.block_Vs[block_Num]:
+
+                if V == variable:
+                    sum += value
+                    num_Assignment2 += 1
+                elif V in self.game_kenken.get_partially_assigned_vars_dict():
+                    sum += self.game_kenken.get_partially_assigned_vars_dict()[V]
+                    num_Assignment2 += 1
+
+            # we check if the sum is equal it the required result or not 
+             # we check if the number of the var we assinged equal to the number of var or not 
+   
+            if sum == self.block_Val[block_Num] and num_Assignment2 == len(self.block_Vs[block_Num]):
+                    return True
+            # we check if the sum is less it the required result or not      
+            # we check if the number of the var we assinged less to the number of var or not 
+  
+            elif sum <= self.block_Val[block_Num] and num_Assignment2 < len(self.block_Vs[block_Num]):
+                    return True
+            else:
+                return False
+    
+        elif self.block_Op[block_Num] == 'mult':
+            mult2 = 1
+            num_Assignment2 = 0
+            
+            # we go through to each var and in the block 
+            # and check if it has on value in his domain 
+            # if it's we just mult it, if not we don't return value of it
+            for V in self.block_Vs[block_Num]:
+            
+                if V == variable:
+                    mult2 *= value
+                    num_Assignment2 += 1
+                elif V in self.game_kenken.get_partially_assigned_vars_dict():
+                    mult2 *= self.game_kenken.get_partially_assigned_vars_dict()[V]
+                    num_Assignment2 += 1
+
+            # we check if the mult is equal it the required result or not
+            # we check if the number of the var we assinged equal to the number of var or not 
+            if mult2 == self.block_Val[block_Num] and num_Assignment2 == len(self.block_Vs[block_Num]):
+                    return True
+
+            # we check if the mult is less it the required result or not
+            # we check if the number of the var we assinged less to the number of var or not 
+            elif mult2 <= self.block_Val[block_Num] and num_Assignment2 < len(self.block_Vs[block_Num]):
+                    return True
+            else:
+                return False
+    
+        elif self.block_Op[block_Num] == 'div':
+            for V in self.block_Vs[block_Num]:
+                if V != variable:
+                    constraint_Var2 = V
+    
+            if constraint_Var2 in self.game_kenken.get_partially_assigned_vars_dict():
+                constraint_Val2 = self.game_kenken.get_partially_assigned_vars_dict()[constraint_Var2]
+                value= max(constraint_Val2, value) / min(constraint_Val2, value)
+                res=value == self.block_Val[block_Num]
+                return res
+            else:
+                for d in self.game_kenken.values_options(constraint_Var2):
+                    x=max(d, value) / min(d, value)
+                    if x == self.block_Val[block_Num]:
+                        return True
+    
+                return False
+        elif self.block_Op[block_Num] == 'sub':
+            for x in self.block_Vs[block_Num]:
+                if x != variable:
+                    constraint_Var2 = x
+    
+            if constraint_Var2 in self.game_kenken.get_partially_assigned_vars_dict():
+                constraint_Val2 = self.game_kenken.get_partially_assigned_vars_dict()[constraint_Var2]
+                temp = max(constraint_Val2, value) - min(constraint_Val2, value)
+                res = temp == self.block_Val[block_Num]
+                return res
+            else:
+                for x in self.game_kenken.values_options(constraint_Var2):
+                    temp = max(x, value) - min(x, value)
+                    if temp  == self.block_Val[block_Num]:
+                        return True
+    
+                return False	
+    
+    def display(self, dic, size):
+        for i in range(size):
+            for j in range(size):
+                string = 'K' + str(i) + str(j)
+                sys.stdout.write(str(dic[string]) + " ")
+            print()
+    
+def getLines(size):
+        lines = []
+        rarr = randomize_grid.randomize_grid(size)
+        cages = randomize_grid.randomize_cages(rarr)
+
+        for cage in cages:
+            var = cage[0]
+            op= cage[1]
+            val = cage[2]
+            strVar = str(var)
+            l = strVar.split()
+            varSt = ""
+            for l1 in l:
+                varSt += l1 
+            # varSt.join(l)
+            # print(varSt)
+            lines.append( varSt + " "+ str(op) + " " + str(val) + "\n" )
+        
+        return lines
+       
+def gather(iterations, out):
+  
+            bt         = lambda ken: csp.backtracking_search(game_kenken)
+            # bt_mrv     = lambda ken: csp.backtracking_search(game_kenken, select_unassigned_variable=csp.mrv)
+            fc         = lambda ken: csp.backtracking_search(game_kenken, inference=csp.forwardCheckingFn)
+            # fc_mrv     = lambda ken: csp.backtracking_search(game_kenken, inference=csp.forwardCheckingFn, select_unassigned_variable=csp.mrv)
+            mac        = lambda ken: csp.backtracking_search(game_kenken, inference=csp.mac)
+            # mconflicts = lambda ken: csp.min_conflicts(ken)
+
+            algorithms = {
+                "BT": bt,
+                # "BT+MRV": bt_mrv,
+                "FC": fc,
+                # "FC+MRV": fc_mrv,
+                "MAC": mac,
+                # "MIN_CONFLICTS": mconflicts
+            }
+
+            with open(out, "w+") as file:
+
+                out = writer(file)
+
+                out.writerow(["Algorithm","Size","Avg Time Per Iteration"])    
+
+
+                for name, algorithm in algorithms.items():
+                    for size in range(3, 10):
+                        checks, assignments, dt = (0, 0, 0)
+                        for iteration in range(1, iterations + 1):
+                        
+                            # V=randomize_grid(size)
+                            # lines =randomize_cages(V)
+                            lines = getLines(size) 
+                            # print(lines)
+                            kenken=KenKen(size, lines)   
+                            game_kenken = csp.Constrain_Satsified_Problem(kenken.vars, kenken.domains, kenken.adjecent, kenken.constraint)
+                            kenken.insertGame(game_kenken)
+                            assignment, time = benchmark(game_kenken, algorithm)
+                        
+                        print("algorithm =",  name, "size =", size, "iteration =", iteration, "result =", "Success" if assignment else "Failure", file=stderr)
+                        time = time / iterations
+                        out.writerow([name, size, time])
+
+
+
+
+def benchmark(kenken, algorithm):
+
+
+        dt = time()
+
+        assignment = algorithm(kenken)
+
+        dt = time() - dt
+
+        return assignment,  dt
+
+		
+		
+		
+if __name__ == '__main__':
+    
+
+    gather(3, "ken.csv")
